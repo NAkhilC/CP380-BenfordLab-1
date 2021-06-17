2b64c11105a1bfb15384f2448f5dc47daf45afb1
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2021-06-17 17:42:23 -0400

    Update Benford.cs

--- BenfordLab/Benford.cs
+++ BenfordLab/Benford.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace BenfordLab
 {
@@ -10,13 +11,15 @@ namespace BenfordLab
         public int Digit { get; set; }
         public int Count { get; set; }
 
-        public BenfordData() { }
+        public BenfordData()
+        { }
     }
 
     public class Benford
     {
-       
-        public static BenfordData[] calculateBenford(string csvFilePath)
+        public static IEnumerable<BenfordData> D2 { get; private set; }
+
+        public static BenfordData[] CalculateBenford(string csvFilePath)
         {
             // load the data
             var data = File.ReadAllLines(csvFilePath)
@@ -26,22 +29,40 @@ namespace BenfordLab
                 {
                     Country = data.Groups[1].Value,
                     Population = int.Parse(data.Groups[2].Value)
+                    
                 });
+            int ki = 0;
+            foreach (var s in data)
+            {
+                ki++;
+            }
 
-            // manipulate the data!
-            //
-            // Select() with:
-            //   - Country
-            //   - Digit (using: FirstDigit.getFirstDigit() )
-            // 
-            // Then:
-            //   - you need to count how many of *each digit* there are
-            //
-            // Lastly:
-            //   - transform (select) the data so that you have a list of
-            //     BenfordData objects
-            //
-            var m = ??? ;
+            int[] arr = new int[ki];
+            int j = 0;
+            foreach (var s in data)
+            {
+              arr[j]=FirstDigit.getFirstDigit(s.Population);
+                j++;    
+            }
+            List<BenfordData> D = new List<BenfordData>();
+            for (int i = 1; i < 10; i++)
+            {
+                int x1 = 0;
+                for(int x=0;x<arr.Length;x++)
+                {
+                    if( i ==arr[x])
+                    {
+                        x1++;
+                    }
+                }
+                D.Add(new BenfordData
+                {
+                    Digit = i,
+                    Count = x1
+                }) ;
+                D.Concat(D);
+            }
+            var m = D;
 
             return m.ToArray();
         }
